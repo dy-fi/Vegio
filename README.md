e95c1dac35afb9076db9ce2dcd9cc686bce2a3e2
Dylan Finn <dylanfinn89@gmail.com>
2020-03-05 14:39:39 -0800

    heroku push

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 A vegetarian restaurant locator and review platform
 
 [Proposal](proposal.md)
+
+I couldn't get Django to deploy to Heroku or DO, even after dockerizing
+
+:(
+
+2 apps in project: 40
+Modular design: 25
+Restful API using Django: 25
--- vegio/accounts/views.py
+++ vegio/accounts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
+from recipes.models import Recipe 
 
 class SignupView(CreateView):
     form_class = 'django.auth.forms.UserCreationForm'
     template_name = 'registration/signup.html'
+
+class ListRecipes(ListView):
+    template_name = 'registration/list.html'
+    model = Recipe
+    paginate_by = 10
+
+    def get_queryset(self, **kwargs):
+        
